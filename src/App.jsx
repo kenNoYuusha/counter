eb73077bcf68b693b8a6e2c56450cbbf5ecde2f8
Ren Amaki <jorge_luis1709@hotmail.com>
2022-11-30 16:25:22 -0600

    se agrego porcentaje

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,39 @@ const App = () => {
   });
 
   useEffect(() => {
-
     const counter = JSON.parse(localStorage.getItem("COUNTER"));
     if (counter) {
-      setState({...counter, confirm: false})
+      setState({ ...counter, confirm: false });
     } else {
       localStorage.setItem("COUNTER", JSON.stringify({ yes: 0, no: 0 }));
-      setState({yes: 0, no: 0, confirm: false})
+      setState({ yes: 0, no: 0, confirm: false });
     }
   }, []);
 
   const { yes, no, confirm } = state;
+  let porcentaje;
+  if (yes == 0 && no > 0) {
+    porcentaje = -100;
+  } else if (yes == 0 && no == 0) {
+    porcentaje = 0;
+  } else {
+    porcentaje = (100 - (no * 100) / yes).toFixed(0);
+  }
 
   const onYes = () => {
     setState({ ...state, yes: yes + 1 });
-    localStorage.setItem('COUNTER', JSON.stringify({yes: yes + 1, no}))
+    localStorage.setItem("COUNTER", JSON.stringify({ yes: yes + 1, no }));
   };
   const onNo = () => {
     setState({ ...state, no: no + 1 });
-    localStorage.setItem('COUNTER', JSON.stringify({yes, no: no + 1}))
+    localStorage.setItem("COUNTER", JSON.stringify({ yes, no: no + 1 }));
   };
   const onConfirm = () => {
     setState({ ...state, confirm: true });
   };
   const onReset = () => {
     setState({ ...state, yes: 0, no: 0, confirm: false });
-    localStorage.setItem('COUNTER', JSON.stringify({yes: 0, no: 0}))
+    localStorage.setItem("COUNTER", JSON.stringify({ yes: 0, no: 0 }));
   };
   const onCancel = () => {
     setState({ ...state, confirm: false });
@@ -41,20 +48,29 @@ const App = () => {
   return (
     <main
       className="w-full h-screen grid place-items-center 
-                     bg-zinc-800 text-zinc-100"
+                     bg-zinc-800 text-zinc-100 font-Kalam"
     >
-      <div className="w-4/5 h-4/5 flex flex-col items-center justify-evenly text-2xl">
-        <h2>Releases</h2>
+      <div className="w-full auto flex flex-col items-center justify-start gap-8 text-3xl bg-zinc-800">
+        <h2>
+          Releases{" "}
+          <span
+            className={`font-bold ${
+              porcentaje < 80 ? "text-red-500" : "text-emerald-500"
+            }`}
+          >
+            {porcentaje}%
+          </span>
+        </h2>
         <button
           className="w-32 h-32 grid place-items-center
-                         bg-emerald-800 border-4 border-zinc-100 rounded-full text-3xl"
+                         bg-emerald-700 hover:bg-emerald-800 active:bg-emerald-900 border-4 border-zinc-100 rounded-full text-3xl"
           onClick={onYes}
         >
           {yes}
         </button>
         <button
           className="w-32 h-32 grid place-items-center
-                           bg-red-800 border-4 border-zinc-100 rounded-full text-3xl"
+                           bg-red-700 hover:bg-red-800 active:bg-red-900  border-4 border-zinc-100 rounded-full text-3xl"
           onClick={onNo}
         >
           {no}
@@ -62,7 +78,7 @@ const App = () => {
 
         {!confirm && (
           <button
-            className="px-10 py-4 bg-cyan-800 border-4 border-zinc-100 rounded-xl"
+            className="px-10 py-4 bg-cyan-700 hover:bg-cyan-800 active:bg-cyan-900 border-4 border-zinc-100 rounded-xl text-2xl"
             onClick={onConfirm}
           >
             Reset
@@ -74,13 +90,13 @@ const App = () => {
             <p>Are you Sure?</p>
             <div className="flex flex-row gap-6">
               <button
-                className="px-8 py-2 bg-zinc-800 border-4 border-zinc-100 rounded-xl text-xl"
+                className="px-8 py-2 bg-zinc-700 hover:bg-zinc-800 active:bg-zinc-900 border-4 border-zinc-100 rounded-xl text-xl"
                 onClick={onCancel}
               >
                 Cancel
               </button>
               <button
-                className="px-8 py-2 bg-cyan-800 border-4 border-zinc-100 rounded-xl text-xl"
+                className="px-8 py-2 bg-cyan-700 hover:bg-cyan-800 active:bg-cyan-900  border-4 border-zinc-100 rounded-xl text-xl"
                 onClick={onReset}
               >
                 OK
